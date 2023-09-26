ebfc7844fdfcc023dd2e14e7def1dd8411ce09fd
gsusk <satehortuamercado@gmail.com>
2023-09-26 00:08:52 -0500

    initial commit

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -22,20 +22,47 @@ class Cursor {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
   }
 
-  up() {
-    // Move cursor up
+  up = () => {
+    this.resetBackgroundColor();
+    if (this.row !== 0) {
+      this.row--;
+    }
+
+    this.setBackgroundColor();
+    Screen.render()
   }
 
   down() {
-    // Move cursor down
+    this.resetBackgroundColor();
+
+    if (this.row !== this.numRows - 1) {
+      this.row++;
+    }
+
+    this.setBackgroundColor();
+    Screen.render();
   }
 
-  left() {
-    // Move cursor left
+  left = () => {
+    this.resetBackgroundColor();
+
+    if (this.col !== 0) {
+      this.col--;
+    }
+
+    this.setBackgroundColor()
+    Screen.render()
   }
 
-  right() {
-    // Move cursor right
+  right = () => {
+    this.resetBackgroundColor();
+
+    if (this.col !== this.numCols - 1) {
+      this.col++;
+    }
+
+    this.setBackgroundColor
+    Screen.render()
   }
 
 }
